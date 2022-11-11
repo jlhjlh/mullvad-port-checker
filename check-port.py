85d1c4f90db88b6789d00f5bd1009c8f402a12a6
Jacob Hileman <jhileman@crp.org>
2022-11-10 23:57:24 -0500

    initial commit

---
+++ check-port.py
@@ -0,0 +1,41 @@
+import json
+import os
+import subprocess
+
+from dotenv import load_dotenv
+import requests
+
+
+# docker exec qbittorrent curl -s https://am.i.mullvad.net/port/56812
+
+###############################################
+# initialize vars and get the script rolling!
+###############################################
+
+# load .env vars which contains my tokens
+load_dotenv()
+PUSHOVER_USER_TOKEN = os.getenv("PUSHOVER_USER_TOKEN")
+PUSHOVER_API_TOKEN = os.getenv("PUSHOVER_API_TOKEN")
+PORT = os.getenv("PORT")
+
+
+result = subprocess.run(
+    f"/usr/bin/docker exec qbittorrent curl -s https://am.i.mullvad.net/port/{PORT}",
+    shell=True,
+    stdout=subprocess.PIPE,
+)
+mullvad_response = json.loads(result.stdout)
+print(mullvad_response)
+
+if mullvad_response["reachable"] == False:
+    print(f"Mullvad port {PORT} is closed.")
+    
+    url = "https://api.pushover.net/1/messages.json"
+    params = {
+        "token": PUSHOVER_API_TOKEN,
+        "user": PUSHOVER_USER_TOKEN,
+        "message": f"Mullvad port {PORT} is closed.",
+    }
+
+    pushover_response = requests.post(url, params=params)
+    print(f"Response from Pushover: {pushover_response.text}")
